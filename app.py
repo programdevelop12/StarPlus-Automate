aa13500bd5d3afe2f3a8a8da2319ee850cbfe014
Lukas Lima <github.lukasdelevops217.negativing@aleeas.com>
2021-10-10 17:25:05 -0300

    Enter

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 import time
 
-def bot_stream():
-    driver = webdriver.Firefox(executable_path=r'./geckodriver.exe')
-    driver.get("https://www.starplus.com/pt-br")
-    driver.find_element_by_xpath('/html/body/header/nav[1]/a').click()
+driver = webdriver.Firefox(executable_path=r'./geckodriver.exe')
+driver.get("https://www.starplus.com/pt-br")
+time.sleep(5)
+driver.find_element_by_xpath('/html/body/header/nav[1]/a').click()
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="email"]').send_keys('vavabill@gmail.com')
+time.sleep(5)
+driver.find_element_by_xpath('/html/body/div/div/div[4]/div/main/div/form/div[1]/button').click()
 
 #/html/body/header/nav[1]/a
-    
+
+#/html/body/div/div/div[4]/div/main/div/form/div[1]/button
